77fa0f9a8c7158d6296eec38933a8184a30a6ffd
Tim Carman <tpcarman@gmail.com>
2024-06-05 13:02:03 +1000

    Create Install-WinUtils.ps1

---
+++ Install-WinUtils.ps1
@@ -0,0 +1,11 @@
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install PowerShell 7
+choco install powershell --version=7.1.3 -y
+
+# Install VS Code
+choco install vscode -y
+
+# Add PowerShell 7 to PATH
+[System.Environment]::SetEnvironmentVariable('PATH', $env:PATH + ';C:\Program Files\PowerShell\7', [System.EnvironmentVariableTarget]::Machine)
