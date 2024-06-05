f9093509bda48712e6ab86b30962b9a801aa45e2
Tim Carman <tpcarman@gmail.com>
2024-06-05 14:52:53 +1000

    Update Install-WinUtils.ps1

--- Install-WinUtils.ps1
+++ Install-WinUtils.ps1
@@ -7,5 +7,14 @@ choco install powershell-core -y
 # Install VS Code
 choco install vscode -y
 
+# Install RVTools
+choco install RVTools -y
+
 # Add PowerShell 7 to PATH
 [System.Environment]::SetEnvironmentVariable('PATH', $env:PATH + ';C:\Program Files\PowerShell\7', [System.EnvironmentVariableTarget]::Machine)
+
+# Install PowerShell Modules
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module -Name ImportExcel -Repository PSGallery -SkipPublisherCheck -AllowClobber -Force -Confirm:$false
+Install-Module -Name Az -Repository PSGallery -SkipPublisherCheck -AllowClobber -Force -Confirm:$false
+Install-Module -Name VMware.PowerCLI -Repository PSGallery -SkipPublisherCheck -AllowClobber -Force -Confirm:$false
